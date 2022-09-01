c4388770d2280b8cc6133ac799cf77b3e5b38570
LordNik10 <niccolonaso@hotmail.com>
2022-09-01 17:04:16 +0200

    end

--- js/index.js
+++ js/index.js
@@ -22,62 +22,6 @@ function changeback() {
   }
 }
 
-var t1 = 1;
-function next() {
-  console.log("next");
-  var p = document.getElementById("imgpart");
-  if (t1 < 5) {
-    t1++;
-  } else {
-    t1 = 1;
-  }
-
-  switch (t1) {
-    case 1:
-      p.src = "./images/tim.jpg";
-      break;
-    case 2:
-      p.src = "./images/expert.png";
-      break;
-    case 3:
-      p.src = "./images/telecom.png";
-      break;
-    case 4:
-      p.src = "./images/sky.png";
-      break;
-    case 5:
-      p.src = "./images/apple1.png";
-      break;
-  }
-}
-
-function prev() {
-  console.log("prev");
-  var p = document.getElementById("imgpart");
-  if (t1 > 1) {
-    t1--;
-  } else {
-    t1 = 5;
-  }
-  switch (t1) {
-    case 1:
-      p.src = "./images/tim.jpg";
-      break;
-    case 2:
-      p.src = "./images/expert.png";
-      break;
-    case 3:
-      p.src = "./images/telecom.png";
-      break;
-    case 4:
-      p.src = "./images/sky.png";
-      break;
-    case 5:
-      p.src = "./images/apple1.png";
-      break;
-  }
-}
-
 function openmenu() {
   var m = document.getElementById("navhamb");
   m.style.display = "block";
@@ -91,3 +35,7 @@ function closemenu() {
   var x = document.getElementById("bdy");
   x.classList.remove("op");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  start();
+});
